e28ed6f739d5b4f2aaa16eed952914ce36fb2bb2
illispi <44943503+illispi@users.noreply.github.com>
2023-12-26 10:33:34 +0200

    progress

--- src/components/ThemeToggleButton.tsx
+++ src/components/ThemeToggleButton.tsx
@@ -1,4 +1,4 @@
-import { Index, type Component, createEffect } from "solid-js";
+import { Index, type Component, createEffect, Show } from "solid-js";
 
 const ThemeToggleButton: Component<{}> = (props) => {
   const rootEl =
@@ -30,44 +30,45 @@ const ThemeToggleButton: Component<{}> = (props) => {
 
   return (
     <div class="theme-toggle">
+      <Show
+        when={themeCur === "light"}
+        fallback={
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
+          </svg>
+        }
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+            clip-rule="evenodd"
+          />
+        </svg>
+      </Show>
+
       <Index each={themes}>
         {(theme) => (
-          <label class={themeCur === theme ? "checked" : ""}>
-            <Show
-              when={theme === "light"}
-              fallback={
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
-                </svg>
-              }
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-            </Show>
+          <label class={themeCur === theme() ? "checked" : ""}>
             <input
               type="radio"
-              name="theme-toggle"
-              checked={theme === theme}
-              value={theme}
-              title={`Use ${theme} theme`}
-              aria-label={`Use ${theme} theme`}
+              name="theme()-toggle"
+              checked={themeCur === theme()}
+              value={theme()}
+              title={`Use ${theme()} theme`}
+              aria-label={`Use ${theme()} theme`}
               onChange={handleChange}
             />
           </label>
@@ -76,3 +77,5 @@ const ThemeToggleButton: Component<{}> = (props) => {
     </div>
   );
 };
+
+export default ThemeToggleButton;
