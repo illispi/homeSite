05cd39204d61f13585d52577c4b499b4d7e00be1
illispi <44943503+illispi@users.noreply.github.com>
2024-11-19 09:34:14 +0200

    astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,28 @@
-import { defineConfig } from "astro/config";
-import solid from "@astrojs/solid-js";
 import mdx from "@astrojs/mdx";
+import solid from "@astrojs/solid-js";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
+import rehypeExternalLinks from "rehype-external-links";
 import remarkGfm from "remark-gfm";
 import remarkSmartypants from "remark-smartypants";
-import rehypeExternalLinks from "rehype-external-links";
-import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://delvis.org",
-  integrations: [mdx(), solid(), tailwind()],
-  output: "static",
-  markdown: {
-    // shikiConfig: {
-    //   theme: "nord",
-    // },
-    remarkPlugins: [remarkGfm, remarkSmartypants],
-    rehypePlugins: [
-      [
-        rehypeExternalLinks,
-        {
-          target: "_blank",
-        },
-      ],
-    ],
-  },
+	site: "https://delvis.org",
+	integrations: [mdx(), solid(), tailwind()],
+	output: "static",
+	markdown: {
+		// shikiConfig: {
+		//   theme: "nord",
+		// },
+		remarkPlugins: [remarkGfm, remarkSmartypants],
+		rehypePlugins: [
+			[
+				rehypeExternalLinks,
+				{
+					target: "_blank",
+				},
+			],
+		],
+	},
 });
